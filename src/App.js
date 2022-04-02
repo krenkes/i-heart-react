a2f35ba64efd920e912f6a823e337bacd743c304
Kevin Renkes <kevinrenkes@gmail.com>
2022-04-02 10:34:13 -0500

    Task 1 Part 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import HeartLogo from './heart.svg';
 import './App.css';
+import Heart from './components/Heart';
+import Header from './components/Header';
 
 const message = 'cool cud';
 
-const Heart = (props) => {
-  return (
-    <div className='heart'>
-      <img className='heart-img' src={HeartLogo} alt='heart' />
-      <p className='heart-message'>{props.msg}</p>
-    </div>
-  );
-}
-
-const Header = () => {
-  return <div className='App-header'>I ❤️ React</div>;
-};
 
 function App() {
   return (
