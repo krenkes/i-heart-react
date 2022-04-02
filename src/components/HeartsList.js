0a711111c63f3e0e66c3374cedfb1c37eb11c3c9
Kevin Renkes <kevinrenkes@gmail.com>
2022-04-02 12:01:52 -0500

    Task 3 completed

--- src/components/HeartsList.js
+++ src/components/HeartsList.js
@@ -1,4 +1,5 @@
-import Heart from './Heart';
+import CandyColoredHeart from './CandyColoredHeart';
+
 
 const messages = [
     'cool cud',
@@ -31,7 +32,7 @@ const messages = [
 
 const HeartsList = () => {
     return <div className='hearts-container'>
-        {messages.map(message => <Heart msg={message} />)}
+        {messages.map(message => <CandyColoredHeart msg={message} />)}
     </div>;
 };
 export default HeartsList;
